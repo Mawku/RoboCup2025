ce0e809a311bf43bb780eb20c09c9204bc5c88cb
Kusby <77204821+Mawku@users.noreply.github.com>
2025-03-13 13:57:55 +0100

    Update finale1.ino

--- finale1.ino
+++ finale1.ino
@@ -26,9 +26,7 @@ void forward(){
 
 void back() {
   analogWrite(ENA, 100);
-  analogWrite(ENB, 100
-  
-  );
+  analogWrite(ENB, 100);
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
